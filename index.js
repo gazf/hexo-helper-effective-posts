48d66aae4cb0efd36551bf664a25cfbf1909a67a
gazf <gazfff@gmail.com>
2016-08-23 16:13:25 +0900

    Update index.js

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 
 var helper = hexo.extend.helper;
 if (helper.register.length === 1){
-  helper.register(require('./lib/main'))
+  helper.register(require('./lib/'))
 } else {
-  helper.register('effective_posts', require('./lib/main'));
-  helper.register('effe', require('./lib/main'));
+  helper.register('effective_posts', require('./lib/'));
+  helper.register('effe', require('./lib/'));
 }
